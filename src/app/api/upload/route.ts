344e8e5f983abc1d59bdeb99222b2ca3aedcfd82
kokily <kokily@naver.com>
2025-07-08 19:56:29 +0900

    Axios Upload test

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -4,38 +4,43 @@ import moment from 'moment';
 import sharp from 'sharp';
 
 export async function POST(req: NextRequest) {
-  const data = await req.formData();
-  const file: File | null = data.get('file') as unknown as File;
-
-  const client = new S3Client({
-    region: 'ap-northeast-2',
-    credentials: {
-      accessKeyId: process.env.ACCESS_KEY_ID!,
-      secretAccessKey: process.env.SECRET_ACCESS_KEY!,
-    },
-  });
-
-  const originalFilename = file.name.replaceAll('_', '');
-  const updateFileName = `${moment().format('YYYYMMDDHHmm')}${originalFilename.trim()}`;
-  const fileName = updateFileName.split('.')[0];
-
-  if (!file) {
-    throw new Error('파일 업로드 실패');
-  } else {
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
-    const body = await sharp(buffer).webp().toBuffer();
-
-    const command = new PutObjectCommand({
-      Bucket: process.env.S3_BUCKET,
-      Key: fileName, 
-      Body: body,
+  try {
+    const data = await req.formData();
+    const file: File | null = data.get('file') as unknown as File;
+  
+    const client = new S3Client({
+      region: 'ap-northeast-2',
+      credentials: {
+        accessKeyId: process.env.ACCESS_KEY_ID!,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY!,
+      },
     });
-
-    await client.send(command);
-
-    return NextResponse.json({
-      url: `https://${process.env.S3_BUCKET}/${fileName}`,
-    });
+  
+    const originalFilename = file.name.replaceAll('_', '');
+    const updateFileName = `${moment().format('YYYYMMDDHHmm')}${originalFilename.trim()}`;
+    const fileName = updateFileName.split('.')[0];
+  
+    if (!file) {
+      throw new Error('파일 업로드 실패');
+    } else {
+      const bytes = await file.arrayBuffer();
+      const buffer = Buffer.from(bytes);
+      const body = await sharp(buffer).webp().toBuffer();
+  
+      const command = new PutObjectCommand({
+        Bucket: process.env.S3_BUCKET,
+        Key: fileName, 
+        Body: body,
+      });
+  
+      await client.send(command);
+  
+      return NextResponse.json({
+        url: `https://${process.env.S3_BUCKET}/${fileName}`,
+      });
+    }  
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: '파일 업로드 실패' }, { status: 500 });
   }
 }
